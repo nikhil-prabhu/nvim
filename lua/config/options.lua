d67b983bb09390abc67167676d555407fb3d3dea
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2025-09-23 18:36:22 +0200

    feat: add config options for neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@
 vim.opt.relativenumber = false
 vim.opt.number = true
 vim.opt.autochdir = true
+
+-- Configuration options for Neovide
+if vim.g.neovide then
+  vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h14" }
+  vim.g.neovide_refresh_rate = 60
+end
